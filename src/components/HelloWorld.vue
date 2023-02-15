fdfb2042cb8c9d16701cfdc502ddab2c535bf157
kangyaxin <kangyaxin@huice.com>
2023-02-15 19:52:17 +0800

    颜色选择器色系值调整

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -411,6 +411,23 @@ const sliderMarks = ref({
   90: '90',
   100: '100',
 })
+const colorValue = ref('')
+const predefineColors = ref([
+  '#ff4500',
+  '#ff8c00',
+  '#ffd700',
+  '#90ee90',
+  '#00ced1',
+  '#1e90ff',
+  '#c71585',
+  'rgba(255, 69, 0, 0.68)',
+  'rgb(255, 120, 0)',
+  'hsv(51, 100, 98)',
+  'hsva(120, 40, 94, 0.5)',
+  'hsl(181, 100%, 37%)',
+  'hsla(209, 100%, 56%, 0.73)',
+  '#c7158577',
+])
 </script>
 
 <template>
@@ -632,7 +649,9 @@ const sliderMarks = ref({
         :options="cascaderOptions" 
       ></el-cascader>
     </el-form-item>
-    <el-form-item label="颜色选择器" v-if="false"></el-form-item>
+    <el-form-item label="ColorPicke">
+      <el-color-picker v-model="colorValue" show-alpha :predefine="predefineColors" />
+    </el-form-item>
     <el-form-item label="Rate">
       <el-rate
         v-model="rateValue"
