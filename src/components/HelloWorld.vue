bdf0d29af8fa38dcabadb816499dfd541b21a92e
kangyaxin <kangyaxin@huice.com>
2023-02-16 16:13:38 +0800

    popover色系值调整

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import { ElMessage } from 'element-plus'
-import { ArrowDown, Eleme, Search, StarFilled } from '@element-plus/icons-vue'
+import { ArrowDown, Eleme, Search, StarFilled, WarningFilled } from '@element-plus/icons-vue'
 import { ElNotification } from 'element-plus'
 
 defineProps<{ msg: string }>();
@@ -786,8 +786,121 @@ const transferData = ref<Option[]>(generateData())
     </el-form-item>
     <el-form-item label="文件上传" v-if="false"></el-form-item>
     <!-- ------------------------------------------------------------ -->
-    <el-form-item label="文字提示" v-if="false"></el-form-item>
-    <el-form-item label="气泡提示" v-if="false"></el-form-item>
+    <el-form-item label="Tooltip">
+      <el-tooltip
+        effect="dark"
+        content="Top Left prompts info"
+        placement="top-start"
+        trigger="click"
+      >
+        <el-button>top-start</el-button>
+      </el-tooltip>
+      <el-tooltip
+        effect="dark"
+        content="Top Center prompts info"
+        placement="top"
+      >
+        <el-button>top</el-button>
+      </el-tooltip>
+      <el-tooltip
+        effect="dark"
+        content="Top Right prompts info"
+        placement="top-end"
+      >
+        <el-button>top-end</el-button>
+      </el-tooltip>
+      <el-tooltip
+        effect="dark"
+        content="Left Top prompts info"
+        placement="left-start"
+      >
+        <el-button>left-start</el-button>
+      </el-tooltip>
+      <el-tooltip
+        effect="dark"
+        content="Right Top prompts info"
+        placement="right-start"
+      >
+        <el-button>right-start</el-button>
+      </el-tooltip>
+      <el-tooltip
+        effect="dark"
+        content="Left Center prompts info"
+        placement="left"
+      >
+        <el-button class="mt-3 mb-3">left</el-button>
+      </el-tooltip>
+      <el-tooltip
+        effect="dark"
+        content="Right Center prompts info"
+        placement="right"
+      >
+        <el-button>right</el-button>
+      </el-tooltip>
+      <el-tooltip
+        class="box-item"
+        effect="dark"
+        content="Left Bottom prompts info"
+        placement="left-end"
+      >
+        <el-button>left-end</el-button>
+      </el-tooltip>
+      <el-tooltip
+        class="box-item"
+        effect="dark"
+        content="Right Bottom prompts info"
+        placement="right-end"
+      >
+        <el-button>right-end</el-button>
+      </el-tooltip>
+      <el-tooltip
+        class="box-item"
+        effect="dark"
+        content="Bottom Left prompts info"
+        placement="bottom-start"
+      >
+        <el-button>bottom-start</el-button>
+      </el-tooltip>
+      <el-tooltip
+        class="box-item"
+        effect="dark"
+        content="Bottom Center prompts info"
+        placement="bottom"
+      >
+        <el-button>bottom</el-button>
+      </el-tooltip>
+      <el-tooltip
+        class="box-item"
+        effect="dark"
+        content="Bottom Right prompts info"
+        placement="bottom-end"
+      >
+        <el-button>bottom-end</el-button>
+      </el-tooltip>
+    </el-form-item>
+    <el-form-item label="Popover">
+      <el-popover
+        placement="top-end"
+        title="标题内容"
+        trigger="click"
+        content="需勾选表格项后进行批量删除文字文字中"
+      >
+        <template #reference>
+          <el-button plain>click to activate</el-button>
+        </template>
+      </el-popover>
+      <el-popconfirm
+        confirm-button-text="确定"
+        cancel-button-text="取消"
+        :icon="WarningFilled"
+        icon-color="#ED6A0C"
+        title="确认解除绑定该店铺吗？"
+      >
+        <template #reference>
+          <el-button plain>Delete</el-button>
+        </template>
+      </el-popconfirm>
+    </el-form-item>
     <el-form-item label="微标数" v-if="false"></el-form-item>
     <el-form-item label="卡片" v-if="false"></el-form-item>
     <el-form-item label="走马灯" v-if="false"></el-form-item>
